6fd7721b9a9330580d0c84a2ba37b9c66216602b
Tapan Nallan <tapan.nallan@verimi.com>
2020-05-06 00:39:41 +0200

    custom fish prompt based on env context

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -14,13 +14,16 @@ function fish_prompt
 	set -l blue (set_color blue)
 	set -l cyan (set_color cyan)
 	set -l gray (set_color 555)
+	set -l orange (set_color orange)
 
 	set -l cwd (prompt_pwd)
 
+	set -l lambda "߷"
+
 	if test $last_status -eq 0
-		set status_indicator $green"➤"
+		set status_indicator $green$lambda
 	else
-		set status_indicator $red"➤"
+		set status_indicator $red$lambda
 	end
 
 	if [ (_rider_theme_git_branch_name) ]
@@ -30,13 +33,21 @@ function fish_prompt
 		else if test (_rider_theme_git_branch_name) = 'develop'
 			set -l git_branch (_rider_theme_git_branch_name)
 			set git_info "$cyan ($yellow$git_branch$cyan)$normal"
+		else if test (_rider_theme_git_branch_name) = 'release'
+			set -l git_branch (_rider_theme_git_branch_name)
+			set git_info "$cyan ($orange$git_branch$cyan)$normal"
 		else
 			set -l git_branch (_rider_theme_git_branch_name)
 			set git_info "$cyan ($green$git_branch$cyan)$normal"
 		end
 	end
 
-	set -l lambda "߷"
-
-	echo "$green$USER $magenta$lambda $blue$cwd $status_indicator$git_info $normal"
+	switch $ENV_CONTEXT
+		case 'VERIMI'
+			echo "$status_indicator$magenta [$ENVNUM]$blue$cwd$git_info"
+		case 'PERSONAL'
+			echo "$green$USER $magenta$lambda $blue$cwd $status_indicator$git_info $normal"
+		case '*'
+			echo "$green$USER $magenta$lambda $blue$cwd $status_indicator$git_info $normal"
+	end
 end
